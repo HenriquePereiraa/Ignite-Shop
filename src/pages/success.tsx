7349943815e5ecabb09e094a262e1d5a16edabf2
Henrique Pereira <henrique802011@gmail.com>
2023-01-23 21:14:04 -0300

    refactor confirmed purchase page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,18 +1,25 @@
 import { stripe } from "@/lib/stripe";
 import Stripe from "stripe";
 
-import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import {
+  ImageContainer,
+  ImageSlideWrapper,
+  SuccessContainer,
+} from "@/styles/pages/success";
 
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
+// import { useContext } from "react";
+// import { ShoppingCartContext } from "@/context/ShoppingCartContext";
+import { useKeenSlider } from "keen-slider/react";
 
 interface SuccessProps {
   customerName: string;
   product: {
     name: string;
-    imageUrl: string;
+    imagesUrl: string[];
   };
 }
 
@@ -28,13 +35,21 @@ export default function Success({ customerName, product }: SuccessProps) {
       <SuccessContainer>
         <h1>Compra efetuada</h1>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
+        <ImageSlideWrapper>
+          {product.imagesUrl.map((image) => {
+            return (
+              <ImageContainer key={image.image}>
+                <Image src={image.image} width={120} height={110} alt="" />
+              </ImageContainer>
+            );
+          })}
+        </ImageSlideWrapper>
 
         <p>
-          Uhuul <strong>{customerName}</strong>, sua{" "}
-          <strong>{product.name}</strong> já está a caminho de sua casa
+          Uhuul <strong>{customerName}</strong>, sua compra de {product.imagesUrl.length >= 1
+            ? `${product.imagesUrl.length} camisetas`
+            : `${product.imagesUrl.length} camiseta`}{" "}
+          já está a caminho de sua casa
         </p>
 
         <Link href="/">Voltar ao catálogo</Link>
@@ -62,12 +77,19 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const customerName = session.customer_details;
   const product = session.line_items?.data[0].price?.product as Stripe.Product;
 
+  // PEGAR OS DADOS RETORNADOS DO STRIPE E ENVIAR PARA AS PROPS
+  const ImagesProducts = session.line_items?.data?.map((item) => {
+    return {
+      image: item.price?.product.images[0],
+    };
+  });
+
   return {
     props: {
       customerName: customerName?.name,
       product: {
         name: product?.name,
-        imageUrl: product?.images[0],
+        imagesUrl: ImagesProducts,
       },
     },
   };
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -36,14 +36,21 @@ export const SuccessContainer = styled("main", {
   },
 });
 
+export const ImageSlideWrapper = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 export const ImageContainer = styled("div", {
   width: "100%",
-  maxWidth: 130,
-  height: 145,
+  maxWidth: 145,
+  height: 140,
   background: "linear-gradient(180deg, #1ea486 0%, #7465d4 100%)",
-  borderRadius: 8,
+  borderRadius: "50%",
   padding: "0.25rem",
   marginTop: "4rem;",
+  marginLeft: "-50px",
 
   display: "flex",
   alignItems: "center",
